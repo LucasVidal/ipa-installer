86f20791599e0cb02b779b3763697d6b58dbba13
Devs Restorando <devsrestorando@Devss-Mac-mini.local>
2015-09-22 11:12:24 -0300

    change endpoint

--- app.rb
+++ app.rb
@@ -11,10 +11,10 @@ class App < Sinatra::Base
   configure do
     #The public addres where the devices will be downloading the ipas. 
     #Must have ssl enabled
-    set :ipa_url_prefix, "https://dl.dropboxusercontent.com/u/22381470/releases/"
+    set :ipa_url_prefix, "https://dl.dropboxusercontent.com/u/22381470/restobuilds/"
 
     #The local folder where the ipas are located
-    set :local_folder, "/Users/lucas/Dropbox/Public/releases" 
+    set :local_folder, "/Users/devsrestorando/Dropbox/builds"
 
     #plist metadata
     set :bundle_id, "com.restorando.iphone"
---
+++ start_server.sh
@@ -0,0 +1,2 @@
+cd /Users/devsrestorando/workspace/ipa-installer;
+rackup -o 0.0.0.0 -p 9201 -d
